6b4dce6925671eacf9c152a38262ae9c0ca61975
dany <dany@petra.ac.id>
2021-04-13 13:22:37 +0700

    versi3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -4,7 +4,10 @@ import { Observable } from 'rxjs';
 import { AngularFirestoreCollection } from '@angular/fire/firestore';
 import { AngularFirestore } from '@angular/fire/firestore'
 import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { ActivatedRoute } from '@angular/router';
+import { FotoService } from '../service/photoservice.service';
+import { Url } from 'node:url';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -18,16 +21,43 @@ export class Tab3Page {
   isi : string
   date : Date
   rating : string
+  urlImageStorage  : any[] =[];
   photolist : string[] = []
-  constructor(afs : AngularFirestore,public router : Router,private route: ActivatedRoute) {
+  constructor(public fotoservice : FotoService,private afStorage : AngularFireStorage,afs : AngularFirestore,public router : Router,private route: ActivatedRoute) {
     this.isidataCol = afs.collection('databaru')
     this.isidata = this.isidataCol.valueChanges();
+   
+  
   }
   ngOnInit() {
     this.route.params.subscribe(async params => {
         this.judul = params['judul']; 
-        console.log(params['judul'])
+        
     });
+  }
+  getdata(){
+    return this.isidataCol.doc<data>(this.judul).valueChanges()
+  }
+  show(temp : string[]){
+      
+      for (var index in temp){
+
+        const imgFilePath = `imgStorage/${temp}`
+        //console.log(index)
+        var storage = this.afStorage.storage;
+        var gsReference =storage.refFromURL('gs://bucket/images/'+temp[index])
+        this.urlImageStorage.unshift(gsReference)
+       
+      }
+  }
+  edit(){
+    console.log(this.judul)
+    this.isidataCol.doc(this.judul).update({
+      isi : this.isi
+    });
+  }
+  delete(){
+    this.isidataCol.doc(this.judul).delete();
   }
 }
 interface data{
