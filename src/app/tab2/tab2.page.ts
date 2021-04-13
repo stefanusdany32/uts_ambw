28719fcd30de58ef7749944bd4c018587542f1a5
dany <dany@petra.ac.id>
2021-04-13 12:06:40 +0700

    halaman 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  isidata : Observable<data[]>;
+  isidataCol : AngularFirestoreCollection<data>;
+  constructor(afs : AngularFirestore,public router : Router) {
+    this.isidataCol = afs.collection('databaru')
+    this.isidata = this.isidataCol.valueChanges();
 
-  constructor() {}
-
+  }
+  pindah(judul : string){
+    console.log("pindah")
+    this.router.navigate(['/tab3', judul]);
+  }
+}
+interface data{
+  judul : string,
+  isi : string,
+  date : Date,
+  rating : string,
+  foto : string[]
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore'
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
-  constructor() {}
-
+  judul : string
+  isidata : Observable<data[]>;
+  isidataCol : AngularFirestoreCollection<data>;
+  jdl : string
+  isi : string
+  date : Date
+  rating : string
+  photolist : string[] = []
+  constructor(afs : AngularFirestore,public router : Router,private route: ActivatedRoute) {
+    this.isidataCol = afs.collection('databaru')
+    this.isidata = this.isidataCol.valueChanges();
+  }
+  ngOnInit() {
+    this.route.params.subscribe(async params => {
+        this.judul = params['judul']; 
+        console.log(params['judul'])
+    });
+  }
+}
+interface data{
+  judul : string,
+  isi : string,
+  date : Date,
+  rating : string,
+  foto : string[]
 }
